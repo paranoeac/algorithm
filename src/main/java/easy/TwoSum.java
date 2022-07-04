d1d78001f2d28c8c3cdd2464f7820c13b9918c19
paranoeac <paranoeac@gamil.com>
2022-07-04 11:01:27 +0900

    init.

---
+++ src/main/java/easy/TwoSum.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class TwoSum {
+
+    public int[] solution(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]) {
+                    return new int[]{nums[i], nums[j]};
+                }
+            }
+        }
+
+        return null;
+    }
+}
