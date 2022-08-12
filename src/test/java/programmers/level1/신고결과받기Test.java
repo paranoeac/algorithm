7fdba4ca45da9dd796272f5afea985120c5cfe99
paranoeac <paranoeac@gamil.com>
2022-08-12 13:48:17 +0900

    프로그래머스 Level1

---
+++ src/test/java/programmers/level1/신고결과받기Test.java
@@ -0,0 +1,11 @@
+package programmers.level1;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+class 신고결과받기Test {
+
+    @Test
+    void solution() {
+    }
+}
